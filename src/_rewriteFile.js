fc7a11f45e33fc1e5927f8cd98540faf90ddb6b3
Liu Maumasi <liu.maumasi@gmail.com>
2019-09-29 23:59:47 -0500

    updating README file

--- src/_rewriteFile.js
+++ src/_rewriteFile.js
@@ -4,12 +4,12 @@ const fs = require('fs');
 const buildRelativePath = require('./buildRelativePath');
 
 // util
-function rewriteFile({ finalDest, src, dest, libFile }, content) {
+function rewriteFile({ finalDest, src }, content) {
   fs.writeFile(finalDest, content, (err) => {
     if(err) {
         console.log(err);
     }
-      console.log(`Library created: ${cliColor.blueBright(buildRelativePath(src, dest, libFile))});
+      console.log(`Library created: ${cliColor.blueBright(src)});
   }); // write to file
 }
 
